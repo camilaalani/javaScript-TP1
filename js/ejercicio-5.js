9f7b2eb5467476026bdfe6635701a7bb80caabab
Camila Alani <alanicamila124@gmail.com>
2023-10-24 20:57:31 -0300

    agregue dos variables pidiendole al usuario 1 numero para que al sumarlos, el resultado se muestre por pantalla con un texto

---
+++ js/ejercicio-5.js
@@ -0,0 +1,6 @@
+/* 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+*/
+
+let numero1 = prompt("Ingrese un número");
+let numero2 = prompt("Ingrese otro número");
+document.write("El resultado de la suma es: "+ (parseInt(numero1) + parseInt(numero2)));
