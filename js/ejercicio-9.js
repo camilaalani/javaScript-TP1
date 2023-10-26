a29b16b303b639f2758427ff15696ca8b40addf5
Camila Alani <alanicamila124@gmail.com>
2023-10-25 23:58:51 -0300

    agregue un bucle en el que se le pide al usuario que ingrese una frase y luego se muestra por pantalla la cantidad de vocales que escribio

--- js/ejercicio-9.js
+++ js/ejercicio-9.js
@@ -1,12 +1,12 @@
-/* 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen.
-Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
-*/
+// 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen.
+// Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
 
 let frase = prompt("Ingrese una frase");
-console.log(frase.length)
-console.log(frase.charAt(0));
+let nFrase = frase.length;
+let i;
 
-
-if(frase.charAt(0) === "a" || frase.charAt(0) === "e" || frase.charAt(0) === "i" || frase.charAt(0) === "o" || frase.charAt(0) === "u"){
-    document.write(frase.charAt(0))
+for(i= 0; i < nFrase; i++){
+    if(frase.charAt(i,1) === "a" || frase.charAt(i,1) === "e" || frase.charAt(i,1) === "i" || frase.charAt(i,1) === "o" || frase.charAt(i,1) === "u"){
+        document.write(frase.charAt(i,1));
+    }
 }
